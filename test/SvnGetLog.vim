61e1b647d2261818e6ed12e64ef8792d266355b1
katonori <katonori.d@gmail.com>
2014-07-09 01:28:19 +0900

    added a test.

---
+++ test/SvnGetLog.vim
@@ -0,0 +1,11 @@
+:SvnGetLog
+:w! res0-0.txt
+:SvnGetLogNextRange
+:w! res0-1.txt
+:SvnGetLogNextRange
+:w! res0-2.txt
+:SvnGetLogPrevRange
+:w! res0-3.txt
+:SvnGetLogPrevRange
+:w! res0-4.txt
+:q
---
+++ test/run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+rm -fv repos/res*.txt
+cp *.vim repos
+pushd repos
+vim -c ":source SvnGetLog.vim"
+popd
+mv repos/res*.txt .
+
+rm -fv res.txt
+for file in res*-*.txt
+do
+    diff ${file} ref/${file} | tee -a res.txt
+done
+LINE=`cat res.txt`
+if [ "" = "${LINE}" ]; then
+    echo "OK"
+    exit 0
+else
+    echo "NG"
+    exit 1
+fi
